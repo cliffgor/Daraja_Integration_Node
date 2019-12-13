7546ba4c1612ccd5fd16d5843f500fb8e829da93
Cliff Gor <clierozigy@gmail.com>
2019-12-13 16:38:02 +0300

    simulating mpesa transactions

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const request = require("request");
 const app = express();
+const bodyParser = require("body-parser");
 
 // Routes
 app.get("/", (req, res) => {
@@ -8,9 +9,70 @@ app.get("/", (req, res) => {
 });
 
 // Acess Token Route
-app.get("/acess_token", (req, res) => {});
+app.get("/access_token", access, (req, res) => {
+  res.status(200).json({ access_token: req.access_token });
+});
 
-function acess(res, req, next) {
+app.get("/register", access, (req, resp) => {
+  let url = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl";
+  let auth = "Bearer" + req.access_token;
+
+  request(
+    {
+      url: url,
+      method: "POST",
+      headers: {
+        Authorization: auth
+      },
+      json: {
+        ShortCode: "600329",
+        ResponseType: "Complete",
+        ConfirmationURL: "https://f44fee8b.ngrok.io/confirmation",
+        ValidationURL: "https://f44fee8b.ngrok.io/validation_url"
+      }
+    },
+    function(error, response, body) {
+      if (error) {
+        console.log(error);
+
+        resp.status(200).json(body);
+      }
+    }
+  );
+});
+
+app.post("/confirmation", (req, res) => {
+  console.log("................ confirmation .................");
+  console.log(req.body);
+});
+
+app.post("/validation", (req, resp) => {
+  console.log("................ validation .................");
+  console.log(req.body);
+});
+
+app.get("/simulate", access, (req, res) => {
+  let url = "";
+  let auth = "Bearer" + req.access_token;
+
+  request({
+    url: url,
+    method: "POST",
+    headers: {
+      Authorization: auth
+    },
+    json: {},
+    function(error, response, body) {
+      if (error) {
+        console.log(error);
+      } else {
+        res.status(200).json(body);
+      }
+    }
+  });
+});
+
+function access(req, res, next) {
   // acess token
   let url =
     "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
@@ -29,7 +91,8 @@ function acess(res, req, next) {
       if (error) {
         console.log(error);
       } else {
-        req.status(200).json(body);
+        req.access_token = JSON.parse(body).access_token;
+        next();
       }
     }
   );
